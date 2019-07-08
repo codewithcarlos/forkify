89ee0b297c878727eda1885f2db238e423f223a9
Carlos Munoz <carlosmunoz@Carloss-MacBook-Pro-4.local>
2019-07-08 15:43:31 -0700

    updated responsiveness

--- starter/src/js/config.js
+++ starter/src/js/config.js
@@ -1,3 +1,2 @@
-// export const key = "30ed869c9510f4110ef818cf1dd41f83";
+export const key = "30ed869c9510f4110ef818cf1dd41f83";
 // export const key = "434a97399cd8fcf71db9e82212e78d76";
-export const key = "441cb3ff15a09d0dda3ba07aea0501d1";
